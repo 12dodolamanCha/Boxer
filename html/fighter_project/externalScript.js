ea70ca8fdd9f9237cc42a431bef94f3144f95c55
12dodolamanCha <odoublecup@gmail.com>
2025-02-25 13:50:31 +0900

    add headline introducing

--- html/fighter_project/externalScript.js
+++ html/fighter_project/externalScript.js
@@ -2,12 +2,33 @@ function clickMe() {
   window.scrollTo(0, 1008);
 }
 
+var target = document.querySelectorAll('#about');
+var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
+var targetID;
+
+// 팝업 열기
+for(var i = 0; i < target.length; i++){
+  target[i].addEventListener('click', function(){
+    targetID = this.getAttribute('href');
+    document.querySelector(targetID).style.display = 'block';
+  });
+}
+
+// 팝업 닫기
+for(var j = 0; j < target.length; j++){
+  btnPopClose[j].addEventListener('click', function(){
+    this.parentNode.parentNode.style.display = 'none';
+  });
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
   const image = document.querySelectorAll(".myImg");
   const modal = document.getElementById("modal");
   const modalImg = document.getElementById("modal_content");
   const span = document.querySelector(".close");
 
+//모달창 띄우기
 image.forEach(img => {
    img.addEventListener("click", function() {
       modal.style.display = "block";
@@ -15,6 +36,7 @@ image.forEach(img => {
    });
 });
 
+//모달창 닫기
 span.addEventListener("click", function() {
     modal.style.display = "none";
   });
