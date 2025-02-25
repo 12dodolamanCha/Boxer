c5ce8df45e6a107c838b7f859257c8ed3c84baf2
12dodolamanCha <odoublecup@gmail.com>
2025-02-25 10:18:35 +0900

    commit complete

--- html/fighter_project/externalScript.js
+++ html/fighter_project/externalScript.js
@@ -2,6 +2,20 @@ function clickMe() {
   window.scrollTo(0, 1008);
 }
 
-function popup(){
-    window.open('this.src');
-}
+document.addEventListener("DOMContentLoaded", function() {
+  const image = document.querySelectorAll(".myImg");
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal_content");
+  const span = document.querySelector(".close");
+
+image.forEach(img => {
+   img.addEventListener("click", function() {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+   });
+});
+
+span.addEventListener("click", function() {
+    modal.style.display = "none";
+  });
+});
